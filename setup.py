d658f3a75dac2fccc5867a827d742a3be229aa73
yangyue <bluelight598@hotmail.com>
2017-06-26 15:30:53 +0800

    basestring support in python3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='ethjsonrpc',
-    version='0.3.4',
+    version='0.3.5',
     description='Ethereum JSON-RPC client',
     long_description=open('README.rst').read(),
     author='ConsenSys',
